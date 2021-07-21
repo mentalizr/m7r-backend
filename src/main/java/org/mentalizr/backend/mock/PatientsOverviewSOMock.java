a0e8cfb989be0d4ba54b8c0b6e8567ee648c06b8
Arthur Picht <picht@arthurpicht.de>
2021-07-21 18:15:33 +0200

    some fixes on PatientOverviewMocks

--- src/main/java/org/mentalizr/backend/mock/PatientsOverviewSOMock.java
+++ src/main/java/org/mentalizr/backend/mock/PatientsOverviewSOMock.java
@@ -19,19 +19,19 @@ public class PatientsOverviewSOMock {
                 "SS",
                 true,
                 "Eine Nachrichtenzeile",
-                "2021-07-08T06:30:00"
+                "2021-07-08"
         );
         patientsOverviewSOList.add(patientOverviewSO);
 
         patientOverviewSO
                 = new PatientOverviewSO(
-                "mock-id-1",
+                "mock-id-2",
                 "Max Mustermann",
                 "MM",
                 false,
                 "Die erste Zeile einer Nachricht",
-                "2021-07-07T20:30:00",
-                true
+                "2021-07-07",
+                false
         );
         patientsOverviewSOList.add(patientOverviewSO);
 
@@ -41,8 +41,8 @@ public class PatientsOverviewSOMock {
                 "Mina Musterfrau",
                 "MM",
                 false,
-                "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.",
-                "2021-07-07T20:30:00",
+                "Lorem ipsum dolor sit amet",
+                "2021-07-07",
                 true
         );
         patientsOverviewSOList.add(patientOverviewSO);
