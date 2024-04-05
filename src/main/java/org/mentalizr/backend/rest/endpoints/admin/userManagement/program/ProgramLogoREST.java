a2f12b3c886c467cd3ab69d087201bbe4844964e
Arthur Picht <picht@arthurpicht.de>
2024-04-05 16:23:38 +0200

    GetProgramDisplayName deleted; ProgramMetaData, ProgramLogo renamed; clean-ups

--- src/main/java/org/mentalizr/backend/rest/endpoints/admin/userManagement/program/ProgramLogoREST.java
+++ src/main/java/org/mentalizr/backend/rest/endpoints/admin/userManagement/program/ProgramLogoREST.java
@@ -1,40 +1,45 @@
 package org.mentalizr.backend.rest.endpoints.admin.userManagement.program;
 
+import de.arthurpicht.utils.core.strings.Strings;
 import de.arthurpicht.webAccessControl.auth.AccessControl;
 import de.arthurpicht.webAccessControl.auth.Authorization;
 import de.arthurpicht.webAccessControl.auth.UnauthorizedException;
 import org.mentalizr.backend.accessControl.roles.Admin;
 import org.mentalizr.backend.applicationContext.ApplicationContext;
+import org.mentalizr.backend.exceptions.M7rNoSuchResourceException;
 import org.mentalizr.backend.exceptions.M7rUnknownEntityException;
-import org.mentalizr.backend.programSOCreator.ProgramMetaDataAdapter;
+import org.mentalizr.backend.rest.RESTException;
 import org.mentalizr.backend.rest.service.Service;
 import org.mentalizr.contentManager.ContentManager;
-import org.mentalizr.contentManager.fileHierarchy.exceptions.ProgramNotFoundException;
+import org.mentalizr.contentManager.exceptions.ContentManagerException;
 import org.mentalizr.contentManager.fileHierarchy.levels.contentRoot.ProgramConf;
-import org.mentalizr.contentManager.programStructure.ProgramStructure;
-import org.mentalizr.serviceObjects.frontend.program.ProgramMetaDataSO;
 
 import javax.servlet.http.HttpServletRequest;
-import javax.ws.rs.*;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
 import javax.ws.rs.core.Context;
-import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 
 @Path("v1")
-public class GetProgramMetaDataREST {
+public class ProgramLogoREST {
 
-    private static final String SERVICE_ID = "/admin/user/program/getMetaData";
+    private static final String SERVICE_ID = "/admin/user/program/logo";
 
     @GET
     @Path(SERVICE_ID + "/{programId}")
-    @Consumes(MediaType.APPLICATION_JSON)
-    @Produces(MediaType.APPLICATION_JSON)
-    public Response getMetaData(
+    @Produces("image/png")
+    public Response getProgramLogo(
             @PathParam("programId") String programId,
             @Context HttpServletRequest httpServletRequest
     ) {
 
         return new Service(httpServletRequest) {
+
             @Override
             protected String getServiceId() {
                 return SERVICE_ID;
@@ -46,19 +51,22 @@ public class GetProgramMetaDataREST {
             }
 
             @Override
-            protected Object workLoad() throws M7rUnknownEntityException {
+            protected FileInputStream workLoad() throws RESTException, M7rNoSuchResourceException, M7rUnknownEntityException {
                 ContentManager contentManager = ApplicationContext.getContentManager();
                 try {
                     ProgramConf programConf = contentManager.getProgramConfig(programId);
-                    return ProgramMetaDataAdapter.getProgramMetaDataSO(programId, programConf);
-                } catch (ProgramNotFoundException e) {
+                    if (Strings.isUnspecified(programConf.getLogo()))
+                        throw new M7rNoSuchResourceException("Program [" + programId + "] has no logo.");
+                    File logo = contentManager.getMediaResource(programId, programConf.getLogo()).toFile();
+                    return new FileInputStream(logo);
+                } catch (ContentManagerException e) {
                     logger.error("Program not found. Cause: " + e.getMessage(), e);
                     throw new M7rUnknownEntityException(e.getMessage(), e);
+                } catch (FileNotFoundException e) {
+                    throw new RESTException(e);
                 }
             }
 
         }.call();
-
     }
-
 }
--- src/main/java/org/mentalizr/backend/rest/endpoints/admin/userManagement/program/ProgramMetaDataREST.java
+++ src/main/java/org/mentalizr/backend/rest/endpoints/admin/userManagement/program/ProgramMetaDataREST.java
@@ -6,10 +6,12 @@ import de.arthurpicht.webAccessControl.auth.UnauthorizedException;
 import org.mentalizr.backend.accessControl.roles.Admin;
 import org.mentalizr.backend.applicationContext.ApplicationContext;
 import org.mentalizr.backend.exceptions.M7rUnknownEntityException;
+import org.mentalizr.backend.programSOCreator.ProgramMetaDataAdapter;
 import org.mentalizr.backend.rest.service.Service;
 import org.mentalizr.contentManager.ContentManager;
 import org.mentalizr.contentManager.fileHierarchy.exceptions.ProgramNotFoundException;
-import org.mentalizr.contentManager.programStructure.ProgramStructure;
+import org.mentalizr.contentManager.fileHierarchy.levels.contentRoot.ProgramConf;
+import org.mentalizr.serviceObjects.frontend.program.ProgramMetaDataSO;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.ws.rs.*;
@@ -18,15 +20,15 @@ import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
 @Path("v1")
-public class GetProgramDisplayNameREST {
+public class ProgramMetaDataREST {
 
-    private static final String SERVICE_ID = "/admin/user/program/getDisplayName";
+    private static final String SERVICE_ID = "/admin/user/program/metaData";
 
     @GET
     @Path(SERVICE_ID + "/{programId}")
     @Consumes(MediaType.APPLICATION_JSON)
     @Produces(MediaType.APPLICATION_JSON)
-    public Response getDisplayName(
+    public Response getMetaData(
             @PathParam("programId") String programId,
             @Context HttpServletRequest httpServletRequest
     ) {
@@ -43,11 +45,11 @@ public class GetProgramDisplayNameREST {
             }
 
             @Override
-            protected Object workLoad() throws M7rUnknownEntityException {
+            protected ProgramMetaDataSO workLoad() throws M7rUnknownEntityException {
                 ContentManager contentManager = ApplicationContext.getContentManager();
                 try {
-                    ProgramStructure programStructure = contentManager.getProgramStructure(programId);
-                    return programStructure.name();
+                    ProgramConf programConf = contentManager.getProgramConfig(programId);
+                    return ProgramMetaDataAdapter.getProgramMetaDataSO(programId, programConf);
                 } catch (ProgramNotFoundException e) {
                     logger.error("Program not found. Cause: " + e.getMessage(), e);
                     throw new M7rUnknownEntityException(e.getMessage(), e);
