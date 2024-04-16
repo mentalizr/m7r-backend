3e3012722f49b630f3d5d3bc18f3d5c38b8f86f2
Fabian Kalk <fabian.kalk@comrize.de>
2024-04-16 10:57:26 +0200

    [BE-4] added wipe and recovery Service

---
+++ src/main/java/org/mentalizr/backend/rest/endpoints/admin/userManagement/userActivity/AddActivitiesREST.java
@@ -0,0 +1,60 @@
+package org.mentalizr.backend.rest.endpoints.admin.userManagement.userActivity;
+
+import de.arthurpicht.webAccessControl.auth.AccessControl;
+import de.arthurpicht.webAccessControl.auth.Authorization;
+import de.arthurpicht.webAccessControl.auth.UnauthorizedException;
+import org.mentalizr.backend.accessControl.roles.Admin;
+import org.mentalizr.backend.exceptions.M7rIllegalServiceInputException;
+import org.mentalizr.backend.rest.service.Service;
+import org.mentalizr.persistence.mongo.activityStatus.ActivityStatusMessageConverter;
+import org.mentalizr.persistence.mongo.activityStatus.ActivityStatusMessageMongoHandler;
+import org.mentalizr.serviceObjects.userManagement.ActivityStatusMessageCollectionSO;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+@Path("v1")
+public class AddActivitiesREST {
+
+    private static final String SERVICE_ID = "/admin/user/activity/add";
+
+    @POST
+    @Path(SERVICE_ID)
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Response add(ActivityStatusMessageCollectionSO activityStatusMessageCollectionSO, @Context HttpServletRequest httpServletRequest) {
+        return new Service(httpServletRequest) {
+
+            @Override
+            protected String getServiceId() {
+                return SERVICE_ID;
+            }
+
+            @Override
+            protected Authorization checkSecurityConstraints() throws UnauthorizedException {
+                return AccessControl.assertValidSession(Admin.ROLE_NAME, httpServletRequest);
+            }
+
+            @Override
+            protected ActivityStatusMessageCollectionSO workLoad() {
+                ActivityStatusMessageMongoHandler.insertMany(
+                        ActivityStatusMessageConverter
+                                .convertActivityList(activityStatusMessageCollectionSO.getCollection()));
+                return activityStatusMessageCollectionSO;
+            }
+
+            @Override
+            protected void updateActivityStatus() {
+                ActivityStatusMessageMongoHandler.insertOne(ActivityStatusMessageConverter
+                        .convert(createMessageObject("userid: "
+                                + activityStatusMessageCollectionSO.getCollection().get(1).getUserId())));
+            }
+        }.call();
+    }
+}
---
+++ src/main/java/org/mentalizr/backend/rest/endpoints/admin/userManagement/userActivity/CountActivitiesREST.java
@@ -0,0 +1,53 @@
+package org.mentalizr.backend.rest.endpoints.admin.userManagement.userActivity;
+
+import de.arthurpicht.webAccessControl.auth.AccessControl;
+import de.arthurpicht.webAccessControl.auth.Authorization;
+import de.arthurpicht.webAccessControl.auth.UnauthorizedException;
+import org.mentalizr.backend.accessControl.roles.Admin;
+import org.mentalizr.backend.rest.service.Service;
+import org.mentalizr.persistence.mongo.activityStatus.ActivityStatusMessageConverter;
+import org.mentalizr.persistence.mongo.activityStatus.ActivityStatusMessageMongoHandler;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+public class CountActivitiesREST {
+
+    private static final String SERVICE_ID = "admin/user/activity/count";
+
+    @GET
+    @Path(SERVICE_ID)
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response wipe(@Context HttpServletRequest httpServletRequest) {
+        return new Service(httpServletRequest) {
+
+            @Override
+            protected String getServiceId() {
+                return SERVICE_ID;
+            }
+
+            @Override
+            protected Authorization checkSecurityConstraints() throws UnauthorizedException {
+                return AccessControl.assertValidSession(Admin.ROLE_NAME, httpServletRequest);
+            }
+
+            @Override
+            protected Long workLoad() {
+                return ActivityStatusMessageMongoHandler.count();
+            }
+
+            @Override
+            protected void updateActivityStatus() {
+                ActivityStatusMessageMongoHandler.insertOne(ActivityStatusMessageConverter
+                        .convert(createMessageObject()));
+            }
+        }.call();
+    }
+}
