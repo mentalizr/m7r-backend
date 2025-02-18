88eb6170700f9439967c82fbde7fc77b996d114e
Fabian K. <fabian.kalk@comrize.de>
2025-02-18 16:00:13 +0100

    [CLI-43] First implementation of the required backend service

---
+++ src/main/java/org/mentalizr/backend/rest/endpoints/admin/userManagement/patient/GetAllQueryPatient.java
@@ -0,0 +1,82 @@
+package org.mentalizr.backend.rest.endpoints.admin.userManagement.patient;
+
+import de.arthurpicht.webAccessControl.auth.AccessControl;
+import de.arthurpicht.webAccessControl.auth.Authorization;
+import de.arthurpicht.webAccessControl.auth.UnauthorizedException;
+import org.mentalizr.backend.accessControl.roles.Admin;
+import org.mentalizr.backend.adapter.PatientRestoreSOAdapter;
+import org.mentalizr.backend.exceptions.*;
+import org.mentalizr.backend.rest.service.Service;
+import org.mentalizr.persistence.rdbms.barnacle.connectionManager.DataSourceException;
+import org.mentalizr.persistence.rdbms.barnacle.connectionManager.EntityNotFoundException;
+import org.mentalizr.persistence.rdbms.barnacle.dao.PatientProgramDAO;
+import org.mentalizr.persistence.rdbms.barnacle.dao.RolePatientDAO;
+import org.mentalizr.persistence.rdbms.barnacle.manual.dao.UserLoginCompositeDAO;
+import org.mentalizr.persistence.rdbms.barnacle.manual.vo.UserLoginCompositeVO;
+import org.mentalizr.persistence.rdbms.barnacle.vo.PatientProgramVO;
+import org.mentalizr.persistence.rdbms.barnacle.vo.RolePatientVO;
+import org.mentalizr.serviceObjects.requestObjects.UserListQuerySO;
+import org.mentalizr.serviceObjects.userManagement.PatientRestoreCollectionSO;
+import org.mentalizr.serviceObjects.userManagement.PatientRestoreSO;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.ws.rs.*;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import java.util.List;
+
+@Path("v1")
+public class GetAllQueryPatient {
+    private static final String SERVICE_ID = "admin/user/patient/query/getAll";
+
+    @POST
+    @Path(SERVICE_ID)
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getAll(UserListQuerySO userListQuerySO,
+                           @Context HttpServletRequest httpServletRequest) {
+
+        return new Service(httpServletRequest) {
+
+            @Override
+            protected String getServiceId() {
+                return "admin/user/patient/query/getAll";
+            }
+
+            @Override
+            protected Authorization checkSecurityConstraints() throws UnauthorizedException {
+                return AccessControl.assertValidSession(Admin.ROLE_NAME, this.httpServletRequest);
+            }
+
+            @Override
+            protected Object workLoad() throws DataSourceException, EntityNotFoundException {
+                List<UserLoginCompositeVO> userLoginCompositeVOs = UserLoginCompositeDAO.findAllPatients();
+                PatientRestoreCollectionSO patientRestoreCollectionSO = new PatientRestoreCollectionSO();
+
+                for (UserLoginCompositeVO userLoginCompositeVO : userLoginCompositeVOs) {
+                    PatientRestoreSO patientRestoreSO = createPatientRestoreSO(userLoginCompositeVO);
+                    patientRestoreCollectionSO.getCollection().add(patientRestoreSO);
+                }
+
+                return patientRestoreCollectionSO;
+            }
+
+            private PatientRestoreSO createPatientRestoreSO(UserLoginCompositeVO userLoginCompositeVO) throws DataSourceException, EntityNotFoundException {
+                String userId = userLoginCompositeVO.getUserId();
+
+                RolePatientVO rolePatientVO = RolePatientDAO.load(userId);
+                PatientProgramVO patientProgramVO = PatientProgramDAO.findByUk_user_id(userId);
+
+                PatientRestoreSO patientRestoreSO = PatientRestoreSOAdapter.from(userLoginCompositeVO);
+
+                patientRestoreSO.setProgramId(patientProgramVO.getProgramId());
+                patientRestoreSO.setBlocking(patientProgramVO.getBlocking());
+                patientRestoreSO.setTherapistId(rolePatientVO.getTherapistId());
+                patientRestoreSO.setProjectId(rolePatientVO.getProjectId());
+
+                return patientRestoreSO;
+            }
+        }.call();
+    }
+}
